4a7dc5cf5c0d708829a86a7de7637039f0358963
codelab265 <mphatsomlenga1@gmail.com>
2024-08-10 11:07:40 +0200

    add slider

--- resources/js/Components/Home/ProgramSlider.jsx
+++ resources/js/Components/Home/ProgramSlider.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+
+import Slider from "react-slick";
 
 const ProgramSlider = () => {
     const programs = [
@@ -39,18 +41,29 @@ const ProgramSlider = () => {
         },
     ];
 
-    const [currentIndex, setCurrentIndex] = useState(1);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    let sliderRef = useRef(null);
 
     const nextSlide = () => {
-        setCurrentIndex((currentIndex + 1) % programs.length);
+        sliderRef.slickNext();
     };
 
     const prevSlide = () => {
-        setCurrentIndex((currentIndex - 1 + programs.length) % programs.length);
+        sliderRef.slickPrev();
     };
 
-    const updateSliderPosition = () => {
-        return { transform: `translateX(${-70 * currentIndex + 15}%)` };
+    var settings = {
+        className: "center slider variable-width",
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        initialSlide: 0,
+        beforeChange: function (old, index) {
+            setCurrentIndex(index);
+        },
     };
 
     return (
@@ -58,7 +71,88 @@ const ProgramSlider = () => {
             <h2 className="text-center text-[#280d4a] font-abhaya text-[64px] font-bold mb-6">
                 Our Programs
             </h2>
-            <div className="relative overflow-hidden w-full mt-[52px]">
+            <div className="slider-container relative">
+                <Slider
+                    ref={(slider) => {
+                        sliderRef = slider;
+                    }}
+                    {...settings}
+                >
+                    {programs.map((program, index) => (
+                        <div
+                            key={index}
+                            className={`w-full px-[60px] box-border transition-all duration-500 ease-in-out ${
+                                index === currentIndex
+                                    ? "transform scale-100 opacity-100"
+                                    : "transform scale-75 opacity-30"
+                            }`}
+                        >
+                            <div className="relative bg-white rounded-[30px] overflow-hidden shadow-lg">
+                                <img
+                                    src={program.image}
+                                    alt={program.title}
+                                    className="w-full h-[617px] object-cover"
+                                />
+                                <div className="absolute bottom-[72px]  right-8 bg-white pt-[58px] pb-[51px] pl-[41px] pr-[37px] rounded-lg shadow w-[444px]">
+                                    <div class="text-black text-[64px] font-bold font-abhaya leading-[51.20px]">
+                                        {program.title}
+                                    </div>
+
+                                    <p className="max-w-[366px] text-black text-base font-normal font-inter leading-normal mt-2">
+                                        {program.description}
+                                    </p>
+                                    <p className="text-black text-xl font-bold font-gilroyBold leading-[30px] mt-[14px]">
+                                        {program.time}
+                                    </p>
+                                </div>
+                            </div>
+                            {index === currentIndex && (
+                                <>
+                                    <button
+                                        className="absolute top-1/2 transform -translate-y-1/2 -left-[102px] bg-white/75 rounded-full w-[102px] h-[102px] flex items-center justify-center shadow border-2 border-[#3C3C3C] z-10"
+                                        onClick={prevSlide}
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            className="w-10 h-10"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M15 19l-7-7 7-7"
+                                            />
+                                        </svg>
+                                    </button>
+                                    <button
+                                        className="absolute top-1/2 transform -translate-y-1/2 -right-[102px] bg-white/75 rounded-full w-[102px] h-[102px] flex items-center justify-center shadow border-2 border-[#3C3C3C] z-10"
+                                        onClick={nextSlide}
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            className="w-10 h-10"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M9 5l7 7-7 7"
+                                            />
+                                        </svg>
+                                    </button>
+                                </>
+                            )}
+                        </div>
+                    ))}
+                </Slider>
+            </div>
+            {/* <div className="relative overflow-hidden w-full mt-[52px]">
                 <div
                     className="flex transition-transform duration-500 ease-in-out"
                     style={updateSliderPosition()}
@@ -136,7 +230,7 @@ const ProgramSlider = () => {
                         </div>
                     ))}
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function MainLayout({ children }) {
-    return <main className="w-full">{children}</main>;
+    return <main className="w-full overflow-hidden">{children}</main>;
 }
 
 export default MainLayout;
